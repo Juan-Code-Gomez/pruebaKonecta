0a0c69a3b55bfccc5e752266fa48faea5dd4f084
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2024-05-23 17:22:46 -0500

    Se crea la estructura de las carpetas y se crea el archivo api donde se utiliza axios y el llamado a la api

---
+++ frontend/src/services/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      return Promise.reject(new Error('Unauthorized'));
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
