4829f5e64df034fa2a71a6ec7df6624b6c3ca2c7
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2024-05-23 14:00:50 -0500

    Se crean la configuracion de la base de datos para la creacion de las tablas con las llaves foraneas

---
+++ backend/models/employee.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const Employee = sequelize.define('Employee', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      position: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hire_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      salary: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+      },
+    });
+  
+    Employee.associate = (models) => {
+      Employee.hasMany(models.Request, {
+        foreignKey: 'employee_id',
+        as: 'requests'
+      });
+    };
+  
+    return Employee;
+  };
--- backend/models/index.js
+++ backend/models/index.js
@@ -6,5 +6,14 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.User = require('./user')(sequelize, Sequelize);
+db.Employee = require('./employee')(sequelize, Sequelize.DataTypes);
+db.Request = require('./request')(sequelize, Sequelize.DataTypes);
+
+Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
 
 module.exports = db;
