35b695d2eb7106fcefd448bb98314d9da9bcc8b6
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2024-05-23 13:04:55 -0500

    Creo la conexion a la base de datos y se confirma de que funcione con un console.log al hacer el metodo sync

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-app.use(express.json())
-
+require("dotenv").config();
+const express = require("express");
+const db = require("./models");
+const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+db.sequelize.sync().then(() => {
+  console.log("Database & tables created!");
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
